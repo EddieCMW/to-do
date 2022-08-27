82cbc5ba15f6de9bda8634794629f24b9be1192c
Eddie Arrison <eddiearrison2@gmail.com>
2022-08-26 20:23:16 -0400

    to do list code

---
+++ todo.py
@@ -0,0 +1,84 @@
+from queue import Empty
+
+choice = str(input("Would you like to see your tasks 'S', add a task 'A', or check off a task? 'C' "))
+choice = choice.upper()
+
+
+
+listoftasks = []
+number = 0
+numlist = []
+
+
+def returnMenu():
+    choice = input("\nWould you like to see your tasks 'S', add a task 'A', or check off a task? 'C' ")
+    choice = choice.upper()
+    if choice == 'S':
+        if not listoftasks:
+            print("\nNo tasks added yet!")
+        showTasks()
+
+    if choice == 'A':
+        addTask()
+    
+    if choice == 'C':
+        completeTask()
+    else:
+        print("\nInvalid input, please try again.")
+        returnMenu()
+
+def showTasks():
+    for (i,item) in enumerate(listoftasks, start=1):
+        print(str(i) + '.', item)
+    returnMenu()
+
+def addTask():
+    print("")
+    add = input("Add a task: ")
+    listoftasks.append(add)
+    print("")
+    showTasks()
+    returnMenu()
+
+def completeTask():
+    print("")
+    for (i,item) in enumerate(listoftasks, start=1):
+        print(str(i) + '.', item)
+    delete = int(input("\nWhich task have you completed? "))
+    listoftasks.pop((delete-1))
+    if not listoftasks:
+        print("\nYou've completed all of your tasks!")
+    else:
+        for (i,item) in enumerate(listoftasks, start=1):
+            print(str(i) + '.', item)
+    returnMenu()
+
+if choice == 'S':
+    if not listoftasks:
+        print("\nNo tasks added yet!")
+        returnMenu()
+    showTasks()
+if choice == 'A':
+    addTask()
+if choice == 'C':
+    if not listoftasks:
+        print("\nYou don't have any tasks to complete!")
+        returnMenu()
+    else:
+        completeTask()
+else:
+    print("\nInvalid input, please try again.")
+    returnMenu()
+
+
+
+
+
+
+
+
+
+
+
+
+
